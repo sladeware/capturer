ef6bb39d4e5eaa09c6d6c7ed5b27d550c4b1d210
Slade Maurer <slade@computer.org>
2015-01-01 14:17:49 +0100

    + Crop & Gray. - Night Threshold. Better logging.

--- capturer.py
+++ capturer.py
@@ -19,10 +19,20 @@ from scipy.linalg import norm
 from scipy import sum, average
 from PIL import Image, ImageChops
 
+# Enable debugging
+DEBUG = False
+
+# HTML template to view recent photos
+INDEX_HTML = '/home/pi/capturer/latest/index.html'
+HEADER = '<p><h1>The last three photos, newest first are given below:</h1><h2></p><br>'
+IMG_SRC1 = '<p><img src=\"./images/'
+IMG_SRC2 = '\"><p><hr>'
+FOOTER = '</h2></body></html>'
+
 # For monitoring setup (gdata)
 EMAIL = ''
 PASSWORD = ''
-SPREADSHEET_KEY = '1jisgXtmj31KXqg-4y_UgXMLREgbCgRznA6FO1IVo5Pc' # key param
+SPREADSHEET_KEY = '' # key param
 WORKSHEET_ID = 'od6' # default
 COLUMN_NAME = socket.gethostname() # Must match the spreadsheet column
 COLUMN_TIMESTAMP = COLUMN_NAME + '-timestamp'
@@ -38,18 +48,20 @@ LOG_FILENAME = "/var/log/capturer.log"
 LOG_LEVEL = logging.INFO  # Could be e.g. "DEBUG" or "WARNING"
 
 # The entropy threshold. Store the file if we are greater or equal to it.
-THRESHOLD_DAY = 4.0
-THRESHOLD_NIGHT = 0.5
-
-# When day and night start. The hour they start. 24 hour time.
-DAY_START_HOUR = 6
-NIGHT_START_HOUR = 19
+THRESHOLD = 0.1
 
 # Sets the ISO to be used for captures. Valid values are: 100, 200, 320, 400, 500, 640, 800..
-ISO = 200
+ISO = 500
 
 # The image resolution in pixels (W x H)
 RESOLUTION = (2592, 1944)
+
+# Cropping constants
+TOP_W = 850
+TOP_H = 700
+BOT_W = 1550
+BOT_H = 1400
+ENABLE_CROPPING = True
 
 # Make a class we can use to capture stdout and sterr in the log
 class MyLogger(object):
@@ -57,12 +69,39 @@ class MyLogger(object):
         """Needs a logger and a logger level."""
         self.logger = logger
         self.level = level
- 
+
 def write(self, message):
     # Only log if there is a message (not just a new line)
     if message.rstrip() != "":
         self.logger.log(self.level, message.rstrip())
- 
+
+class LatestIndexPage(object):
+    def __init__(self):
+        self.first = None
+        self.second = None
+        self.third = None
+
+    def generate_index_html(self, name, timestamp):
+        self.third = self.second
+        self.second = self.first
+        self.first = [name, timestamp]
+        html = HEADER
+        html += self.first[1]
+        html += IMG_SRC1 + self.first[0] + IMG_SRC2
+        if self.second:
+            html += self.second[1]
+            html += IMG_SRC1 + self.second[0] + IMG_SRC2
+        if self.third:
+            html += self.third[1]
+            html += IMG_SRC1 + self.third[0] + IMG_SRC2
+        html += FOOTER
+        try:
+            f = open(INDEX_HTML, 'w')
+            f.write(html)
+            f.close
+        except:
+            logger.info('Problems writing index.html:', INDEX_HTML)
+
 def handler(signum, frame):
     logger.info('Good bye!')
     sys.exit(0)
@@ -70,16 +109,28 @@ def handler(signum, frame):
 def capture(camera):
     stream = io.BytesIO()
     camera.start_preview()
-    time.sleep(2)
     camera.capture(stream, format='jpeg')
     # "Rewind" the stream to the beginning so we can read its content
     stream.seek(0)
-    return Image.open(stream)
+    image = Image.open(stream)
+    w, h = image.size
+    if ENABLE_CROPPING:
+        return image.crop((TOP_W, TOP_H, BOT_W, BOT_H))
+    else:
+        return image
 
-def image_entropy(img):
+def color_histo(img):
     w,h = img.size
     a = np.array(img.convert('RGB')).reshape((w*h,3))
-    h,e = np.histogramdd(a, bins=(16,)*3, range=((0,256),)*3)
+    return np.histogramdd(a, bins=(16,)*3, range=((0,256),)*3)
+
+def gray_histo(img):
+    w,h = img.size
+    a = np.array(img.convert('L')).reshape((w*h))
+    return np.histogramdd(a, bins=(16,), range=((0,256),))
+
+def image_entropy(img):
+    h,e = gray_histo(img)
     prob = h/np.sum(h) # normalize
     prob = prob[prob>0] # remove zeros
     return -np.sum(prob*np.log2(prob))
@@ -101,7 +152,7 @@ def setup(camera):
     # some time to measure the scene and determine appropriate values
     time.sleep(2)
 
-def save(img, value, client):
+def save(img, value, client, latest):
     name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.' + value + '.jpg'
     timestamp = datetime.now().strftime('%m/%d/%Y %H:%M:%S')
     img.save(DIRECTORY + name)
@@ -111,7 +162,7 @@ def save(img, value, client):
         client.InsertRow(row, SPREADSHEET_KEY, WORKSHEET_ID)
     except Exception as e:
         logger.info(e)
-        
+    latest.generate_index_html(name, timestamp)
 
 ## START OF MAIN CODE ##
 # TODO(slade): Clean this up.
@@ -127,7 +178,7 @@ parser.add_argument("-l", "--log", help="file to write log to (default '" + LOG_
 args = parser.parse_args()
 if args.log:
     LOG_FILENAME = args.log
- 
+
 # Configure logging to log to a file, making a new file at midnight and keeping the last 3 day's data
 # Give the logger a unique name (good practice)
 logger = logging.getLogger(__name__)
@@ -148,35 +199,42 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 # Replace stdout with logging to file at INFO level
-sys.stdout = MyLogger(logger, logging.INFO)
+if not DEBUG:
+    sys.stdout = MyLogger(logger, logging.INFO)
 
 # Replace stderr with logging to file at ERROR level
-sys.stderr = MyLogger(logger, logging.ERROR)
+if not DEBUG:
+    sys.stderr = MyLogger(logger, logging.ERROR)
 
 # Setup monitoring
 client = gdata.spreadsheet.service.SpreadsheetsService()
-#client.debug = True
+if DEBUG:
+    client.debug = True
 client.email = EMAIL
 client.password = PASSWORD
 client.source = 'Turtle Monitor Client'
-client.ProgrammaticLogin()
+try:
+    client.ProgrammaticLogin()
+except:
+    logger.info('Cannot login to Google')
+
+# Setup Latest Index Page generator
+latest = LatestIndexPage()
 
 logger.info('Starting at: ' + str(datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
-with picamera.PiCamera() as camera:
-    setup(camera)
-    logger.info('Capturing images.')
-    current = capture(camera)
-    save(current, '0', client)
-    while True:
-        new = capture(camera)
-        threshold_value = compare_images(current, new)
-        hour = int(datetime.now().strftime('%H'))
-        if DAY_START_HOUR <= hour <= NIGHT_START_HOUR:
-            if THRESHOLD_DAY <= threshold_value:
+try:
+    with picamera.PiCamera() as camera:
+        setup(camera)
+        logger.info('Capturing images.')
+        current = capture(camera)
+        save(current, '0', client, latest)
+        while True:
+            new = capture(camera)
+            threshold_value = compare_images(current, new)
+            if THRESHOLD <= threshold_value:
                 current = new
-                save(current, str(threshold_value), client)
-        else:
-            if THRESHOLD_NIGHT <= threshold_value:
-                current = new
-                save(current, str(threshold_value), client)
+                save(current, str(threshold_value), client, latest)
+except:
+    logger.info('Problems setting up camera')
+
 
