3bca4188711d3880635b3eb52a902881a0d1d3f5
Slade Maurer <slademaurer@sshome3.local>
2015-08-17 12:25:18 -0700

    Show the most recent image in the header.

--- snap.py
+++ snap.py
@@ -20,12 +20,22 @@ def get_timestamp(format):
 def save_image(cam, timestamp, width=1080):
     path = IMAGES_DIR + '/' + timestamp + '.jpg'
     cam.save_image(path, width)
+    return '.' + '/' + timestamp + '.jpg'
 
+def gen_header(title, desc, filename, header_filename='/Library/Webserver/Documents/header.html'):
+    header_text = '<html><head><title>' + title + '</title></head><body><h2>' + desc + '</h2><b>'
+    header_text += '<img src="' + filename + '"><b>'
+    file = open(header_filename, 'w')
+    file.write(header_text)
+    file.close()
 
 if __name__ == "__main__":
     try:
         cam = get_cam(os.getenv("DROPCAM_USERNAME"), os.getenv("DROPCAM_PASSWORD"))
         timestamp = get_timestamp('%H:%M:%S_%d-%m-%Y')
-        save_image(cam, timestamp)
+        filename = save_image(cam, timestamp)
+        title = 'SF Living room Cam'
+        desc = 'These are Nestcam still images from our SF living room camera facing the Bay Bridge.'
+        gen_header(title, desc, filename)
     except Exception, err:
         print err
